ed66d8a721cde855086e0f65bac516a496114a55
William Robertson <c.william.robertson@gmail.com>
2023-09-04 18:57:09 +0200

    Remove comments

--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,6 @@ const server = new ApolloServer({
   typeDefs,
 })
 
-// Passing an ApolloServer instance to the `startStandaloneServer` function:
-//  1. creates an Express app
-//  2. installs your ApolloServer instance as middleware
-//  3. prepares your app to handle incoming requests
 const { url } = await startStandaloneServer(server, {
   listen: { port: 4000 },
 })
