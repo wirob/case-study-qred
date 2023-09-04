71e94edea721a7284f639421a8e363fa4121775e
William Robertson <c.william.robertson@gmail.com>
2023-09-04 19:20:33 +0200

    Query resolvers

---
+++ src/resolvers/index.ts
@@ -0,0 +1,8 @@
+import queries from 'resolvers/queries'
+import type { Resolvers } from '__generated__/resolvers-types'
+
+const resolvers: Resolvers = {
+  Query: queries,
+}
+
+export default resolvers
---
+++ src/resolvers/queries/index.ts
@@ -0,0 +1,9 @@
+import type { QueryResolvers } from '__generated__/resolvers-types'
+
+const queries: QueryResolvers = {
+  foo: () => {
+    return { id: '7a643909-32da-418d-a9fe-32b2b1dc9a5b' }
+  },
+}
+
+export default queries
