1505e2c6f7de7fc7612f026013d17250b099a478
William Robertson <c.william.robertson@gmail.com>
2023-09-04 16:53:31 +0200

    Apollo server + schema

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('I am a simple console log')
+import { startStandaloneServer } from '@apollo/server/standalone'
+import { ApolloServer } from '@apollo/server'
+import { readFileSync } from 'fs';
+
+const typeDefs = readFileSync('./schema.graphql', { encoding: 'utf-8' });
+
+const server = new ApolloServer({
+  typeDefs,
+})
+
+// Passing an ApolloServer instance to the `startStandaloneServer` function:
+//  1. creates an Express app
+//  2. installs your ApolloServer instance as middleware
+//  3. prepares your app to handle incoming requests
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+})
+
+console.log(`🚀  Server ready at: ${url}`)
