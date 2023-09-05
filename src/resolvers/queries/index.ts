5b80e0de9c59726bcb860eda15dc49068a815c2c
William Robertson <c.william.robertson@gmail.com>
2023-09-05 19:53:58 +0200

    Add company to Query

--- src/resolvers/queries/index.ts
+++ src/resolvers/queries/index.ts
@@ -1,9 +1,12 @@
-import type { QueryResolvers } from '__generated__/resolvers-types'
+import type { QueryResolvers } from 'src/__generated__/resolvers-types'
 
 const queries: QueryResolvers = {
-  transactions: async (_, { id }, { dataSources }) => {
+  transactions: async (parent, { id }, { dataSources }) => {
     return dataSources.transactionsAPI.getTransactions(id)
   },
+  company: async (parent, { id }, { dataSources }) => {
+    return dataSources.companyAPI.getCompany(id)
+  }
 }
 
 export default queries
