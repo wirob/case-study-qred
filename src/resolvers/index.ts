d3e5380c66d1eb419b129caf79eab07550274921
William Robertson <c.william.robertson@gmail.com>
2023-09-05 19:55:33 +0200

    Company has tansactions resolver

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,8 +1,13 @@
-import queries from 'resolvers/queries'
-import type { Resolvers } from '__generated__/resolvers-types'
+import queries from 'src/resolvers/queries'
+import type { Resolvers } from 'src/__generated__/resolvers-types'
 
 const resolvers: Resolvers = {
   Query: queries,
+  Company: {
+    transactions: (parent, args, { dataSources }) => {
+      return dataSources.transactionsAPI.getCompanyTransactions(parent.id)
+    },
+  },
 }
 
 export default resolvers
