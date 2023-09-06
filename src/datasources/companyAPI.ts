a85de18cfe68970145799d5b818c5df9e46fd7c3
William Robertson <c.william.robertson@gmail.com>
2023-09-06 12:18:44 +0200

    Move companies to own file

--- src/datasources/companyAPI.ts
+++ src/datasources/companyAPI.ts
@@ -1,15 +1,5 @@
+import companies from './companies'
 import type { Company } from 'src/__generated__/resolvers-types'
-
-const companies: Omit<Company, '__typename'>[] = [
-  {
-    id: '4c2a48e2-5585-4463-901e-c0a247556d26',
-    name: 'Acme'
-  },
-  {
-    id: 'e940e673-130a-423f-bd1e-bb471ec13daa',
-    name: 'Emca'
-  },
-]
 
 class CompanyDataSource {
   getCompany(id: string): Company {
@@ -17,4 +7,4 @@ class CompanyDataSource {
   }
 }
 
-export { CompanyDataSource }
+export { CompanyDataSource, companies }
